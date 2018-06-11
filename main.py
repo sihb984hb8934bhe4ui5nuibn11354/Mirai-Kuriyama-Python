883c877b5d22762f4ee9eb4e9a0ccc520021b225
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-06-11 19:21:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -130,9 +130,7 @@ async def on_message(message):
 			await client.send_message(message.channel, "https://discordapp.com/oauth2/authorize?client_id=454018497995997184&permissions=8&scope=bot {}".format(str("<@"+message.author.id+">")))
 		
 	if message.content.lower().startswith("k>ping"):
-		now = datetime.datetime.utcnow()
-		delta = now.message.timestamp
-		await client.send_message(message.channel,'Meu ping atual: {0}'.format(delta(microseconds=1)))
+		await client.send_message(message.channel,'Meu ping atual: {0}'.format(str(client.ping))
 		
 		
 		
