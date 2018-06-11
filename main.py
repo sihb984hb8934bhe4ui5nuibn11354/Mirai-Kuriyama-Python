3011f48195836ca3088d646af7e4733b8e7dbe07
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-06-11 15:08:19 -0300

    Update main.py

--- main.py
+++ main.py
@@ -31,76 +31,83 @@ async def on_ready():
 @client.event
 async def on_message(message):
 	if message.content.lower().startswith("<@454018497995997184>"):
-		await client.send_message(message.channel, "{} -- Meu prefixo atual desse servidor é `k>`".format("<@"+str(message.author.id)+">"))
+		if not "Direct Message" in str(message.channel):
+			await client.send_message(message.channel, "{} -- Meu prefixo atual desse servidor é `k>`".format("<@"+str(message.author.id)+">"))
 	
 	if message.content.lower().startswith("k>secret"):
-		await client.send_message(message.channel, "{} -- ErRoR 4o4".format("<@"+str(message.author.id)+">"))
+		if not "Direct Message" in str(message.channel):
+			await client.send_message(message.channel, "{} -- ErRoR 4o4".format("<@"+str(message.author.id)+">"))
 	
 	if message.content.lower().startswith("k>wiki"):
-		await client.send_message(message.channel, "{} -- Pesquisando na wikipedia, aguarde...".format("<@"+str(message.author.id)+">"))
-		try:
-			argumentos = message.content[7:]
-			sf = wikipedia.page(argumentos)
-			embed = discord.Embed(title="Pesquisa da Wikipedia",description="",color=kcolor)
+		if not "Direct Message" in str(message.channel):
+			await client.send_message(message.channel, "{} -- Pesquisando na wikipedia, aguarde...".format("<@"+str(message.author.id)+">"))
 			try:
-				embed.set_image(url=sf.images[0])
+				argumentos = message.content[7:]
+				sf = wikipedia.page(argumentos)
+				embed = discord.Embed(title="Pesquisa da Wikipedia",description="",color=kcolor)
+				try:
+					embed.set_image(url=sf.images[0])
+				except:
+					pass
+				embed.add_field(name="Título", value=sf.title, inline=False)
+				embed.add_field(name="Link", value=sf.url, inline=False)
+				embed.add_field(name="Conteúdo", value=sf.content[:1000].strip()+"...", inline=False)
+				await client.send_message(message.channel, embed=embed)
 			except:
-				pass
-			embed.add_field(name="Título", value=sf.title, inline=False)
-			embed.add_field(name="Link", value=sf.url, inline=False)
-			embed.add_field(name="Conteúdo", value=sf.content[:1000].strip()+"...", inline=False)
-			await client.send_message(message.channel, embed=embed)
-		except:
-			await client.send_message(message.channel, "{} -- Desculpe mais algo deu errado ou está pagina do wikipédia não existe :tired_face:".format("<@"+str(message.author.id)+">"))
+				await client.send_message(message.channel, "{} -- Desculpe mais algo deu errado ou está pagina do wikipédia não existe :tired_face:".format("<@"+str(message.author.id)+">"))
 	
-	if message.content.startswith('k>ajuda'):
-		await client.send_message(message.channel, "{} -- Enviei meus comandos no seu **privado** :inbox_tray:".format("<@" + str(message.author.id) + ">"))
-
-		ajudaembedf=discord.Embed(color=kcolor,icon_url="https://i.imgur.com/rdm3W9t.png")
-		ajudaembedff=discord.Embed(color=kcolor,icon_url="https://i.imgur.com/rdm3W9t.png")
-		ajudaembedfff=discord.Embed(color=kcolor,icon_url="https://i.imgur.com/rdm3W9t.png")
+		if message.content.startswith('k>ajuda'):
+			if not "Direct Message" in str(message.channel):
 		
-		ajudaembedf.set_author(name="Comandos sobre o discord", icon_url="https://i.imgur.com/rdm3W9t.png")
-		ajudaembedff.set_author(name="Comandos sobre mim", icon_url="https://i.imgur.com/rdm3W9t.png")
-		ajudaembedfff.set_author(name="Comandos diversos", icon_url="https://i.imgur.com/rdm3W9t.png")
-		ajudaembedf.add_field(name="k>avatar `(usuario)`", value="Use para capturar uma imagem de certo perfil", inline=False)
-		ajudaembedff.add_field(name="k>invite", value="Para poder me adicionar em seu servidor", inline=False)
-		ajudaembedff.add_field(name="k>botinfo", value="Para conhecer um pouco mais de mim", inline=False)
-		ajudaembedfff.add_field(name="k>wiki `(enciclopédia)`", value="Faz uma pesquisa na wikipedia", inline=False)
-		ajudaembedfff.add_field(name="k>fofoca", value="Fala de umas fofoquinhas que estão acontecendo `(Indisponível)`", inline=False)
+				await client.send_message(message.channel, "{} -- Enviei meus comandos no seu **privado** :inbox_tray:".format("<@" + str(message.author.id) + ">"))
 
-		#ajudaembed.set_image(url="https://cdn.discordapp.com/attachments/454350443276140586/455106888506540032/Image__198761_1517969068.jpeg")
-		ajudaembedfff.set_footer(text="ATT: DEIVIZIN e CentenoBR")
-		await client.send_message(message.author, embed=ajudaembedf)	
-		await client.send_message(message.author, embed=ajudaembedff)
-		await client.send_message(message.author, embed=ajudaembedfff)
+			ajudaembedf=discord.Embed(color=kcolor,icon_url="https://i.imgur.com/rdm3W9t.png")
+			ajudaembedff=discord.Embed(color=kcolor,icon_url="https://i.imgur.com/rdm3W9t.png")
+			ajudaembedfff=discord.Embed(color=kcolor,icon_url="https://i.imgur.com/rdm3W9t.png")
+		
+			ajudaembedf.set_author(name="Comandos sobre o discord", icon_url="https://i.imgur.com/rdm3W9t.png")
+			ajudaembedff.set_author(name="Comandos sobre mim", icon_url="https://i.imgur.com/rdm3W9t.png")
+			ajudaembedfff.set_author(name="Comandos diversos", icon_url="https://i.imgur.com/rdm3W9t.png")
+			ajudaembedf.add_field(name="k>avatar `(usuario)`", value="Use para capturar uma imagem de certo perfil", inline=False)
+			ajudaembedff.add_field(name="k>invite", value="Para poder me adicionar em seu servidor", inline=False)
+			ajudaembedff.add_field(name="k>botinfo", value="Para conhecer um pouco mais de mim", inline=False)
+			ajudaembedfff.add_field(name="k>wiki `(enciclopédia)`", value="Faz uma pesquisa na wikipedia", inline=False)
+			ajudaembedfff.add_field(name="k>fofoca", value="Fala de umas fofoquinhas que estão acontecendo `(Indisponível)`", inline=False)
+
+			#ajudaembed.set_image(url="https://cdn.discordapp.com/attachments/454350443276140586/455106888506540032/Image__198761_1517969068.jpeg")
+			ajudaembedfff.set_footer(text="ATT: DEIVIZIN e CentenoBR")
+			await client.send_message(message.author, embed=ajudaembedf)	
+			await client.send_message(message.author, embed=ajudaembedff)
+			await client.send_message(message.author, embed=ajudaembedfff)
 		
 	if message.content.lower().startswith("k>avatar"):
-		try:
-			#argumentos_f = message.content[:9]
-			#argumentos = argumentos_f.split()[1]
-			#escolhido = discord.utils.get(client.get_all_members(), id=str(escolhido_random))
+		if not "Direct Message" in str(message.channel):
 			try:
-				escolhido_a = discord.utils.get(client.get_all_members(), id=str(message.mentions[0].id))
-				avatarembed = discord.Embed(title="",color=kcolor,description="[Clique aqui](" + message.author.avatar_url + ") para acessar o link do avatar!")
-				avatarembed.set_author(name=message.author.name)
-				avatarembed.set_image(url=escolhido_a.avatar_url)
-				await client.send_message(message.channel, embed=avatarembed)
-			except:
-				escolhido_a = discord.utils.get(client.get_all_members(), id=str(message.content[9:]))
-				avatarembed = discord.Embed(title="",color=kcolor,description="[Clique aqui](" + message.author.avatar_url + ") para acessar o link do avatar!")
-				avatarembed.set_author(name=message.author.name)
-				avatarembed.set_image(url=escolhido_a.avatar_url)
-				await client.send_message(message.channel, embed=avatarembed)
+				#argumentos_f = message.content[:9]
+				#argumentos = argumentos_f.split()[1]
+				#escolhido = discord.utils.get(client.get_all_members(), id=str(escolhido_random))
+				try:
+					escolhido_a = discord.utils.get(client.get_all_members(), id=str(message.mentions[0].id))
+					avatarembed = discord.Embed(title="",color=kcolor,description="[Clique aqui](" + message.author.avatar_url + ") para acessar o link do avatar!")
+					avatarembed.set_author(name=message.author.name)
+					avatarembed.set_image(url=escolhido_a.avatar_url)
+					await client.send_message(message.channel, embed=avatarembed)
+				except:
+					escolhido_a = discord.utils.get(client.get_all_members(), id=str(message.content[9:]))
+					avatarembed = discord.Embed(title="",color=kcolor,description="[Clique aqui](" + message.author.avatar_url + ") para acessar o link do avatar!")
+					avatarembed.set_author(name=message.author.name)
+					avatarembed.set_image(url=escolhido_a.avatar_url)
+					await client.send_message(message.channel, embed=avatarembed)
 				
-		except:
-			avatarembed = discord.Embed(title="",color=kcolor,description="[Clique aqui](" + message.author.avatar_url + ") para acessar o link do avatar!")
-			avatarembed.set_author(name=message.author.name)
-			avatarembed.set_image(url=message.author.avatar_url)
-			await client.send_message(message.channel, embed=avatarembed)
+			except:
+				avatarembed = discord.Embed(title="",color=kcolor,description="[Clique aqui](" + message.author.avatar_url + ") para acessar o link do avatar!")
+				avatarembed.set_author(name=message.author.name)
+				avatarembed.set_image(url=message.author.avatar_url)
+				await client.send_message(message.channel, embed=avatarembed)
 			
-	if message.content.lower().startswith("k>invite"):
-		await client.send_message(message.channel, "https://discordapp.com/oauth2/authorize?client_id=454018497995997184&permissions=8&scope=bot {}".format(str("<@"+message.author.id+">")))
+		if message.content.lower().startswith("k>invite"):
+			if not "Direct Message" in str(message.channel):
+				await client.send_message(message.channel, "https://discordapp.com/oauth2/authorize?client_id=454018497995997184&permissions=8&scope=bot {}".format(str("<@"+message.author.id+">")))
 		
 			
 			
