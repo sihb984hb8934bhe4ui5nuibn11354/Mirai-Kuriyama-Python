1fb42bb6d48b5dbf278f5afb385df2c8bf346994
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-06-14 19:47:28 -0300

    Update main.py

--- main.py
+++ main.py
@@ -27,16 +27,16 @@ kcolor = 0x7219ff
 
 client = discord.Client()
 
-def twitch():
+async def twitch():
 	while True:
 		#await client.change_presence(game=discord.Game(name='Estou atualmente em {} servidores'.format(str(number)), url='https://www.twitch.tv/deivizin_', type=1))
 		time.sleep(10)
+		await client.change_presence(game=discord.Game(name='Estou atualmente em {} servidores'.format(str(number)), url='https://www.twitch.tv/deivizin_', type=1))
 		
 	
 @client.event
 async def on_ready():
 	thread.start_new_thread(twitch, ())
-	await client.change_presence(game=discord.Game(name='Estou atualmente em {} servidores'.format(str(number)), url='https://www.twitch.tv/deivizin_', type=1))
 	
 	wikipedia.set_lang("pt")
 	number = 0
