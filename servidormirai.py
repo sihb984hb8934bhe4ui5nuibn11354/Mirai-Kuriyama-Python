0b639e87ec3b6effb3bed1d1c8102661c6fdf783
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-07-05 16:30:24 -0300

    Update servidormirai.py

--- servidormirai.py
+++ servidormirai.py
@@ -1,14 +1,47 @@
 import ftplib
 import os
 
-def conectar():
-  username = str(os.environ.get('USERNAME', None))
-  password = str(os.environ.get('PASSWORD', None))
-  ip = str(os.environ.get('IP', None))
-  port = os.environ.get('PORT', None)
-  ftp = ftplib.FTP()
-
-  ftp.connect(ip, port)
-  ftp.login(username, password)
-  ftp.cwd("htdocs")
-  ftp.cwd("arquivos")
+def online():
+  try:
+    username = str(os.environ.get('USERNAME', None))
+    password = str(os.environ.get('PASSWORD', None))
+    ip = str(os.environ.get('IP', None))
+    port = os.environ.get('PORT', None)
+    ftp = ftplib.FTP()
+    ftp.connect(ip, port)
+    ftp.login(username, password)
+    #ftp.cwd("htdocs")
+    #ftp.cwd("arquivos")
+    ftp.quit()
+    return True
+  except:
+    return False
+  
+def salvar(nome_arquivo):
+  try:
+    username = str(os.environ.get('USERNAME', None))
+    password = str(os.environ.get('PASSWORD', None))
+    ip = str(os.environ.get('IP', None))
+    port = os.environ.get('PORT', None)
+    ftp = ftplib.FTP()
+    ftp.connect(ip, port)
+    ftp.login(username, password)
+    ftp.pwd()
+    ftp.cwd("htdocs")
+    ftp.cwd("arquivos")
+    try:
+      ftp.delete(nome_arquivo)
+    except:
+      pass
+    ftp.storbinary('STOR {}'.format(nome_arquivo), open(nome_arquivo,'rb'))
+    
+  except:
+    ftp.pwd()
+    ftp.cwd("htdowcs")
+    ftp.cwd("arquivos")
+    try:
+      ftp.delete(nome_arquivo)
+    except:
+      pass
+    ftp.storbinary('STOR {}'.format(nome_arquivo), open(nome_arquivo,'rb'))
+    
