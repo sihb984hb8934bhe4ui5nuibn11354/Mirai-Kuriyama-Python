2dc4be348d44204067d175190de119fd5ea3771d
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-07-05 16:17:24 -0300

    Update servidormirai.py

--- servidormirai.py
+++ servidormirai.py
@@ -1,1 +1,14 @@
 import ftplib
+import os
+
+def conectar():
+  username = str(os.environ.get('USERNAME', None))
+  password = str(os.environ.get('PASSWORD', None))
+  ip = str(os.environ.get('IP', None))
+  port = os.environ.get('PORT', None)
+  ftp = ftplib.FTP()
+
+  ftp.connect(ip, port)
+  ftp.login(username, password)
+  ftp.cwd("htdocs")
+  ftp.cwd("arquivos")
