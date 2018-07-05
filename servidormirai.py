8805fef12bb8190f5b662ac4b058ff94ad49bec0
sihb984hb8934bhe4ui5nuibn11354 <37244821+sihb984hb8934bhe4ui5nuibn11354@users.noreply.github.com>
2018-07-05 16:54:54 -0300

    Update servidormirai.py

--- servidormirai.py
+++ servidormirai.py
@@ -41,3 +41,49 @@ def salvar(nome_arquivo):
       pass
     ftp.storbinary('STOR {}'.format(nome_arquivo), open(nome_arquivo,'rb'))
     
+def baixar(nome_arquivo_1, nome_saida):
+  try:
+    username = os.environ.get('USERNAME', None)
+    password = os.environ.get('PASSWORD', None)
+    ftp = ftplib.FTP()
+    ftp.connect("ftp.ezyro.com", 21)
+    ftp.login(username, password)
+    ftp.pwd()
+    ftp.cwd("htdocs")
+    ftp.cwd("arquivos")
+    ftp.retrbinary("RETR " + nome_arquivo_1 ,open(nome_saida, 'wb').write)
+  except:
+    ftp.pwd()
+    ftp.cwd("htdowcs")
+    ftp.cwd("arquivos")
+    ftp.retrbinary("RETR " + nome_arquivo_1 ,open(nome_saida, 'wb').write)
+
+def baixar_valor(nome_arquivo_1):
+  try:
+    username = os.environ.get('USERNAME', None)
+    password = os.environ.get('PASSWORD', None)
+    ftp = ftplib.FTP()
+    ftp.connect("ftp.ezyro.com", 21)
+    ftp.login(username, password)
+    ftp.pwd()
+    ftp.cwd("htdocs")
+    ftp.cwd("arquivos")
+    ftp.retrbinary("RETR " + nome_arquivo_1 ,open(nome_arquivo_1, 'wb').write)
+    file = open(nome_arquivo_1, "rb").read()
+    os.remove(nome_arquivo_1)
+    return file
+  except:
+    ftp.pwd()
+    ftp.cwd("htdowcs")
+    ftp.cwd("arquivos")
+    ftp.retrbinary("RETR " + nome_arquivo_1 ,open(nome_arquivo_1, 'wb').write)
+    file = open(nome_arquivo_1, "rb").read()
+    os.remove(nome_arquivo_1)
+    return file
+    
+def criar_arquivo(nome_arquivo_2, valor_f):
+  file = open(nome_arquivo_2, "wb")
+  file.write(valor_f)
+  file.close()
+    
+    
